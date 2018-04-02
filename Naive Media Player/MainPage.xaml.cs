21218e918fd9442f03aa49096c4383f88cb30c38
pinkido <pinkido@126.com>
2018-04-02 12:01:03 +0800

    Done.

--- Naive Media Player/MainPage.xaml.cs
+++ Naive Media Player/MainPage.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.Media.Core;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -25,6 +31,39 @@ namespace Naive_Media_Player
         public MainPage()
         {
             this.InitializeComponent();
+            CustomTitleBar();
+        }
+
+        void CustomTitleBar()
+        {
+            var view = ApplicationView.GetForCurrentView();
+            view.TitleBar.BackgroundColor = Color.FromArgb(255,0,120,215);
+            view.TitleBar.ButtonBackgroundColor= Color.FromArgb(255, 0, 120, 215);
+        }
+
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            picker.FileTypeFilter.Add(".mp3");
+            picker.FileTypeFilter.Add(".mp4");
+
+            StorageFile MediaFile = await picker.PickSingleFileAsync();
+
+            if (MediaFile != null)
+            {
+                MPE.Source = MediaSource.CreateFromStorageFile(MediaFile);
+                MPE.MediaPlayer.Play();
+                
+            }
+            else
+            {
+                TB001.Text = "Something went wrong.";
+            }
         }
     }
 }
